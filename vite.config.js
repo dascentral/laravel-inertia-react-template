1d148f60c28d248a45ed0307fe37a7dd2a8aad74
Douglas Smith <smithda@gmail.com>
2023-09-25 10:42:59 -0400

    Enabling vite-plugin-eslint

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
+    eslintPlugin(),
     laravel({
       input: 'resources/js/app.jsx',
       refresh: true,
