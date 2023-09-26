09c95b1444ffa6d25ef98bfa66a6ba8aacc7a22b
Douglas Smith <smithda@gmail.com>
2023-09-26 11:36:43 -0400

    Removing route() method

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -24,7 +24,7 @@ export default function Login({ status, canResetPassword }) {
   const submit = (e) => {
     e.preventDefault();
 
-    post(route('login'));
+    post('/login');
   };
 
   return (
@@ -85,7 +85,7 @@ export default function Login({ status, canResetPassword }) {
         <div className="mt-4 flex items-center justify-end">
           {canResetPassword && (
             <Link
-              href={route('password.request')}
+              href="/forgot-password"
               className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
             >
               Forgot your password?
